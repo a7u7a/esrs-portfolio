430bab09fb9964c274807789d4e71941a75e79b4
a7u7a <esteban@esrs.co>
2024-07-24 20:27:17 +0200

    Some progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
+import Head from 'next/head'
 import { getAllProjects } from "@/lib/utils";
 import RenderProject from "@/components/render-project";
+import Header from "@/components/header";
 
 export default async function Home() {
   const projects = await getAllProjects();
 
   return (
-    <main>
-      {projects.map((project, i) => (
-        <RenderProject key={i} project={project} />
-        ))}
-    </main>
+    
+      
+
+      <main className="text-[14px]">
+
+        <Header />
+
+        <div className='max-w-[800px] py-[40px] mx-20'>
+
+          <nav className="flex gap-2">
+            <section>Selected projects</section>
+            <section>Publications</section>
+            <section>Clients</section>
+            <section>Services</section>
+            <section>Contact</section>
+          </nav>
+
+          {projects.map((project, i) => (
+            <RenderProject key={i} project={project} />
+          ))}
+        </div >
+      </main>
+    
   );
 }
