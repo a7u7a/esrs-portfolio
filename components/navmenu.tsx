bfb07969eaa5057e899538d7c6740175ee771c0e
a7u7a <esteban@esrs.co>
2024-10-18 13:03:50 +0200

    Navbar

--- components/navmenu.tsx
+++ components/navmenu.tsx
@@ -9,19 +9,19 @@ const NavMenu = () => {
     <nav className="fixed top-2 w-full flex justify-center z-50 ">
       <ul className="list-none flex items-center gap-14 bg-gray-200 p-0.5 rounded-md font-bold">
 
-        <div className="bg-gray-200 px-1.5 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[1px]" onClick={() => router.push('/#selected')}>
+        <div className="bg-gray-200 px-2 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[2px]" onClick={() => router.push('/#selected')}>
           {"Selected"}
         </div>
 
-        <div className="bg-gray-200 px-1.5 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[1px]" onClick={() => router.push('/#experimental')}>
+        <div className="bg-gray-200 px-2 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[2px]" onClick={() => router.push('/#experimental')}>
           {"Experimental"}
         </div>
 
-        <div className="bg-gray-200 px-1.5 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[1px]" onClick={() => router.push('/#publications')}>
+        <div className="bg-gray-200 px-2 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[2px]" onClick={() => router.push('/#publications')}>
           {"Publications"}
         </div>
 
-        <div className="bg-gray-200 px-1.5 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[1px]" onClick={() => router.push('/#contact')}>
+        <div className="bg-gray-200 px-2 rounded hover:bg-white cursor-pointer hover:text-blue-500 pt-[2px]" onClick={() => router.push('/#contact')}>
           {"Contact"}
         </div>
 
