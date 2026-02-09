8ed8f6c16cff8e3a85eddd326eddf8182c24887a
Esteban Serrano <eserrano@designsystems.international>
2026-02-09 11:37:06 -0300

    fix imports

--- components/work/cv.tsx
+++ components/work/cv.tsx
@@ -1,7 +1,7 @@
 import CVPublications from './cv-publications'
 import CVContact from './cv-contact'
 import CVEducation from './cv-education'
-import Footer from '../Footer'
+import Footer from '@/components/Footer'
 import CVServices from "./cv-services"
 import { useMediaQuery } from '@/lib/hooks'
 
