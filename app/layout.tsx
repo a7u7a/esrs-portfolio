c4970f17d87a1f25f0cc725c2dbcfe607093786a
Esteban Serrano <eserrano@designsystems.international>
2026-02-09 11:44:48 -0300

    add vercel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from 'next/font/local'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://esrs.co'),
@@ -36,6 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body
         className={`
         ${mediumFont.variable}
